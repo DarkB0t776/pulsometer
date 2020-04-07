67f38c4d92148df79d173272238820d2612c4e0c
Dima <dima23790@gmail.com>
2020-04-07 19:43:53 +0300

    Added slider

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
-  $('.carousel__inner').slick();
+  $('.carousel__inner').slick({
+    speed: 1000,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="assets/img/left-arrow.png"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="assets/img/right-arrow.png"></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          dots: true,
+          arrows: false,
+        },
+      },
+    ],
+  });
 });
