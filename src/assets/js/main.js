44967cfd74c02429edf5f36b1bad389dfc814bea
Dima <dima23790@gmail.com>
2020-04-08 12:30:15 +0300

    Added tabs

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  //Slider
   $('.carousel__inner').slick({
     speed: 1000,
     prevArrow:
@@ -15,4 +16,36 @@ $(document).ready(function () {
       },
     ],
   });
+
+  //Tabs
+  $('ul.catalog__tabs').on(
+    'click',
+    'li:not(.catalog__tab_active)',
+    function () {
+      $(this)
+        .addClass('catalog__tab_active')
+        .siblings()
+        .removeClass('catalog__tab_active')
+        .closest('div.container')
+        .find('div.catalog__content')
+        .removeClass('catalog__content_active')
+        .eq($(this).index())
+        .addClass('catalog__content_active');
+    }
+  );
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on('click', function (event) {
+        event.preventDefault();
+        $('.catalog-item__content')
+          .eq(i)
+          .toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      });
+    });
+  }
 });
